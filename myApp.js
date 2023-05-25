72ca38d6f3260cefae6a6bb43482d91717d54a0f
EkaterinaSlotina <78050519+EkaterinaSlotina@users.noreply.github.com>
2023-05-25 15:12:52 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,17 @@ let express = require('express');
 let app = express();
 
 
+app.get('/', function routeHandler(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/json', function(req, res) {
+  res.json({ "message": "Hello json" });
+});
+
+
 
 
 
